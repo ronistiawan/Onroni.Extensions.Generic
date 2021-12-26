4caac52fa561e3f0dcbfd1051670cec5476375f2
Roni Stiawan <roni.physics@gmail.com>
2021-12-26 08:13:59 +0700

    Add project files.

---
+++ Onroni.Extensions.Generic/EnumerableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Onroni.Extensions.Generic
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<string> Replace(this IEnumerable<string> list, string stringToReplace, string newString)
+        {
+            var newList = new List<string>();
+
+            foreach(var item in list)
+            {
+                var cleanItem = item.Replace(stringToReplace, newString);
+                newList.Add(cleanItem);
+            }
+
+            return newList;
+        } 
+    }
+}
