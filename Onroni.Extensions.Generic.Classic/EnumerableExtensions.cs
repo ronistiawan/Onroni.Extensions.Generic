66b2f9e580d789f7d3acf063848fb1669e8d4250
Roni Stiawan <roni.physics@gmail.com>
2021-12-28 08:15:53 +0700

    Classic version

---
+++ Onroni.Extensions.Generic.Classic/EnumerableExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onroni.Extensions.Generic.Classic
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<string> Replace(this IEnumerable<string> list, string stringToReplace, string newString)
+        {
+            var newList = new List<string>();
+
+            foreach (var item in list)
+            {
+                var cleanItem = item.Replace(stringToReplace, newString);
+                newList.Add(cleanItem);
+            }
+
+            return newList;
+        }
+
+        public static IEnumerable<string> RemoveString(this IEnumerable<string> list, string stringToRemove)
+        {
+            var newList = new List<string>();
+
+            foreach (var item in list)
+            {
+                var cleanItem = item.Replace(stringToRemove, "");
+                newList.Add(cleanItem);
+            }
+
+            return newList;
+        }
+    }
+}
